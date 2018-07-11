e216cbed175a3cbf6b484c767fe1799141b8bd20
DeepVoltaire <philip@popien.net>
2018-07-11 23:53:51 +0200

    rotation has fillcolor, fillcolor parameter added

--- autoaugment.py
+++ autoaugment.py
@@ -1,6 +1,5 @@
 from PIL import Image, ImageEnhance, ImageOps
 import numpy as np
-import math
 import random
 
 
@@ -17,36 +16,36 @@ class ImageNetPolicy(object):
         >>>     ImageNetPolicy(),
         >>>     transforms.ToTensor()])
     """
-    def __init__(self):
+    def __init__(self, fillcolor=(128, 128, 128)):
         self.policies = [
-            SubPolicy(0.4, "posterize", 8, 0.6, "rotate", 9),
-            SubPolicy(0.6, "solarize", 5, 0.6, "autocontrast", 5),
-            SubPolicy(0.8, "equalize", 8, 0.6, "equalize", 3),
-            SubPolicy(0.6, "posterize", 7, 0.6, "posterize", 6),
-            SubPolicy(0.4, "equalize", 7, 0.2, "solarize", 4),
+            SubPolicy(0.4, "posterize", 8, 0.6, "rotate", 9, fillcolor),
+            SubPolicy(0.6, "solarize", 5, 0.6, "autocontrast", 5, fillcolor),
+            SubPolicy(0.8, "equalize", 8, 0.6, "equalize", 3, fillcolor),
+            SubPolicy(0.6, "posterize", 7, 0.6, "posterize", 6, fillcolor),
+            SubPolicy(0.4, "equalize", 7, 0.2, "solarize", 4, fillcolor),
 
-            SubPolicy(0.4, "equalize", 4, 0.8, "rotate", 8),
-            SubPolicy(0.6, "solarize", 3, 0.6, "equalize", 7),
-            SubPolicy(0.8, "posterize", 5, 1.0, "equalize", 2),
-            SubPolicy(0.2, "rotate", 3, 0.6, "solarize", 8),
-            SubPolicy(0.6, "equalize", 8, 0.4, "posterize", 6),
+            SubPolicy(0.4, "equalize", 4, 0.8, "rotate", 8, fillcolor),
+            SubPolicy(0.6, "solarize", 3, 0.6, "equalize", 7, fillcolor),
+            SubPolicy(0.8, "posterize", 5, 1.0, "equalize", 2, fillcolor),
+            SubPolicy(0.2, "rotate", 3, 0.6, "solarize", 8, fillcolor),
+            SubPolicy(0.6, "equalize", 8, 0.4, "posterize", 6, fillcolor),
 
-            SubPolicy(0.8, "rotate", 8, 0.4, "color", 0),
-            SubPolicy(0.4, "rotate", 9, 0.6, "equalize", 2),
-            SubPolicy(0.0, "equalize", 7, 0.8, "equalize", 8),
-            SubPolicy(0.6, "invert", 4, 1.0, "equalize", 8),
-            SubPolicy(0.6, "color", 4, 1.0, "contrast", 8),
+            SubPolicy(0.8, "rotate", 8, 0.4, "color", 0, fillcolor),
+            SubPolicy(0.4, "rotate", 9, 0.6, "equalize", 2, fillcolor),
+            SubPolicy(0.0, "equalize", 7, 0.8, "equalize", 8, fillcolor),
+            SubPolicy(0.6, "invert", 4, 1.0, "equalize", 8, fillcolor),
+            SubPolicy(0.6, "color", 4, 1.0, "contrast", 8, fillcolor),
 
-            SubPolicy(0.8, "rotate", 8, 1.0, "color", 2),
-            SubPolicy(0.8, "color", 8, 0.8, "solarize", 7),
-            SubPolicy(0.4, "sharpness", 7, 0.6, "invert", 8),
-            SubPolicy(0.6, "shearX", 5, 1.0, "equalize", 9),
-            SubPolicy(0.4, "color", 0, 0.6, "equalize", 3),
+            SubPolicy(0.8, "rotate", 8, 1.0, "color", 2, fillcolor),
+            SubPolicy(0.8, "color", 8, 0.8, "solarize", 7, fillcolor),
+            SubPolicy(0.4, "sharpness", 7, 0.6, "invert", 8, fillcolor),
+            SubPolicy(0.6, "shearX", 5, 1.0, "equalize", 9, fillcolor),
+            SubPolicy(0.4, "color", 0, 0.6, "equalize", 3, fillcolor),
 
-            SubPolicy(0.4, "equalize", 7, 0.2, "solarize", 4),
-            SubPolicy(0.6, "solarize", 5, 0.6, "autocontrast", 5),
-            SubPolicy(0.6, "invert", 4, 1.0, "equalize", 8),
-            SubPolicy(0.6, "color", 4, 1.0, "contrast", 8),
+            SubPolicy(0.4, "equalize", 7, 0.2, "solarize", 4, fillcolor),
+            SubPolicy(0.6, "solarize", 5, 0.6, "autocontrast", 5, fillcolor),
+            SubPolicy(0.6, "invert", 4, 1.0, "equalize", 8, fillcolor),
+            SubPolicy(0.6, "color", 4, 1.0, "contrast", 8, fillcolor)
         ]
 
 
@@ -71,37 +70,37 @@ class CIFAR10Policy(object):
         >>>     CIFAR10Policy(),
         >>>     transforms.ToTensor()])
     """
-    def __init__(self):
+    def __init__(self, fillcolor=(128, 128, 128)):
         self.policies = [
-            SubPolicy(0.1, "invert", 7, 0.2, "contrast", 6),
-            SubPolicy(0.7, "rotate", 2, 0.3, "translateX", 9),
-            SubPolicy(0.8, "sharpness", 1, 0.9, "sharpness", 3),
-            SubPolicy(0.5, "shearY", 8, 0.7, "translateY", 9),
-            SubPolicy(0.5, "autocontrast", 8, 0.9, "equalize", 2),
+            SubPolicy(0.1, "invert", 7, 0.2, "contrast", 6, fillcolor),
+            SubPolicy(0.7, "rotate", 2, 0.3, "translateX", 9, fillcolor),
+            SubPolicy(0.8, "sharpness", 1, 0.9, "sharpness", 3, fillcolor),
+            SubPolicy(0.5, "shearY", 8, 0.7, "translateY", 9, fillcolor),
+            SubPolicy(0.5, "autocontrast", 8, 0.9, "equalize", 2, fillcolor),
 
-            SubPolicy(0.2, "shearY", 7, 0.3, "posterize", 7),
-            SubPolicy(0.4, "color", 3, 0.6, "brightness", 7),
-            SubPolicy(0.3, "sharpness", 9, 0.7, "brightness", 9),
-            SubPolicy(0.6, "equalize", 5, 0.5, "equalize", 1),
-            SubPolicy(0.6, "contrast", 7, 0.6, "sharpness", 5),
+            SubPolicy(0.2, "shearY", 7, 0.3, "posterize", 7, fillcolor),
+            SubPolicy(0.4, "color", 3, 0.6, "brightness", 7, fillcolor),
+            SubPolicy(0.3, "sharpness", 9, 0.7, "brightness", 9, fillcolor),
+            SubPolicy(0.6, "equalize", 5, 0.5, "equalize", 1, fillcolor),
+            SubPolicy(0.6, "contrast", 7, 0.6, "sharpness", 5, fillcolor),
 
-            SubPolicy(0.7, "color", 7, 0.5, "translateX", 8),
-            SubPolicy(0.3, "equalize", 7, 0.4, "autocontrast", 8),
-            SubPolicy(0.4, "translateY", 3, 0.2, "sharpness", 6),
-            SubPolicy(0.9, "brightness", 6, 0.2, "color", 8),
-            SubPolicy(0.5, "solarize", 2, 0.0, "invert", 3),
+            SubPolicy(0.7, "color", 7, 0.5, "translateX", 8, fillcolor),
+            SubPolicy(0.3, "equalize", 7, 0.4, "autocontrast", 8, fillcolor),
+            SubPolicy(0.4, "translateY", 3, 0.2, "sharpness", 6, fillcolor),
+            SubPolicy(0.9, "brightness", 6, 0.2, "color", 8, fillcolor),
+            SubPolicy(0.5, "solarize", 2, 0.0, "invert", 3, fillcolor),
 
-            SubPolicy(0.2, "equalize", 0, 0.6, "autocontrast", 0),
-            SubPolicy(0.2, "equalize", 8, 0.8, "equalize", 4),
-            SubPolicy(0.9, "color", 9, 0.6, "equalize", 6),
-            SubPolicy(0.8, "autocontrast", 4, 0.2, "solarize", 8),
-            SubPolicy(0.1, "brightness", 3, 0.7, "color", 0),
+            SubPolicy(0.2, "equalize", 0, 0.6, "autocontrast", 0, fillcolor),
+            SubPolicy(0.2, "equalize", 8, 0.8, "equalize", 4, fillcolor),
+            SubPolicy(0.9, "color", 9, 0.6, "equalize", 6, fillcolor),
+            SubPolicy(0.8, "autocontrast", 4, 0.2, "solarize", 8, fillcolor),
+            SubPolicy(0.1, "brightness", 3, 0.7, "color", 0, fillcolor),
 
-            SubPolicy(0.4, "solarize", 5, 0.9, "autocontrast", 3),
-            SubPolicy(0.9, "translateY", 9, 0.7, "translateY", 9),
-            SubPolicy(0.9, "autocontrast", 2, 0.8, "solarize", 3),
-            SubPolicy(0.8, "equalize", 8, 0.1, "invert", 3),
-            SubPolicy(0.7, "translateY", 9, 0.9, "autocontrast", 1)
+            SubPolicy(0.4, "solarize", 5, 0.9, "autocontrast", 3, fillcolor),
+            SubPolicy(0.9, "translateY", 9, 0.7, "translateY", 9, fillcolor),
+            SubPolicy(0.9, "autocontrast", 2, 0.8, "solarize", 3, fillcolor),
+            SubPolicy(0.8, "equalize", 8, 0.1, "invert", 3, fillcolor),
+            SubPolicy(0.7, "translateY", 9, 0.9, "autocontrast", 1, fillcolor)
         ]
 
 
@@ -126,37 +125,37 @@ class SVHNPolicy(object):
         >>>     SVHNPolicy(),
         >>>     transforms.ToTensor()])
     """
-    def __init__(self):
+    def __init__(self, fillcolor=(128, 128, 128)):
         self.policies = [
-            SubPolicy(0.9, "shearX", 4, 0.2, "invert", 3),
-            SubPolicy(0.9, "shearY", 8, 0.7, "invert", 5),
-            SubPolicy(0.6, "equalize", 5, 0.6, "solarize", 6),
-            SubPolicy(0.9, "invert", 3, 0.6, "equalize", 3),
-            SubPolicy(0.6, "equalize", 1, 0.9, "rotate", 3),
+            SubPolicy(0.9, "shearX", 4, 0.2, "invert", 3, fillcolor),
+            SubPolicy(0.9, "shearY", 8, 0.7, "invert", 5, fillcolor),
+            SubPolicy(0.6, "equalize", 5, 0.6, "solarize", 6, fillcolor),
+            SubPolicy(0.9, "invert", 3, 0.6, "equalize", 3, fillcolor),
+            SubPolicy(0.6, "equalize", 1, 0.9, "rotate", 3, fillcolor),
 
-            SubPolicy(0.9, "shearX", 4, 0.8, "autocontrast", 3),
-            SubPolicy(0.9, "shearY", 8, 0.4, "invert", 5),
-            SubPolicy(0.9, "shearY", 5, 0.2, "solarize", 6),
-            SubPolicy(0.9, "invert", 6, 0.8, "autocontrast", 1),
-            SubPolicy(0.6, "equalize", 3, 0.9, "rotate", 3),
+            SubPolicy(0.9, "shearX", 4, 0.8, "autocontrast", 3, fillcolor),
+            SubPolicy(0.9, "shearY", 8, 0.4, "invert", 5, fillcolor),
+            SubPolicy(0.9, "shearY", 5, 0.2, "solarize", 6, fillcolor),
+            SubPolicy(0.9, "invert", 6, 0.8, "autocontrast", 1, fillcolor),
+            SubPolicy(0.6, "equalize", 3, 0.9, "rotate", 3, fillcolor),
 
-            SubPolicy(0.9, "shearX", 4, 0.3, "solarize", 3),
-            SubPolicy(0.8, "shearY", 8, 0.7, "invert", 4),
-            SubPolicy(0.9, "equalize", 5, 0.6, "translateY", 6),
-            SubPolicy(0.9, "invert", 4, 0.6, "equalize", 7),
-            SubPolicy(0.3, "contrast", 3, 0.8, "rotate", 4),
+            SubPolicy(0.9, "shearX", 4, 0.3, "solarize", 3, fillcolor),
+            SubPolicy(0.8, "shearY", 8, 0.7, "invert", 4, fillcolor),
+            SubPolicy(0.9, "equalize", 5, 0.6, "translateY", 6, fillcolor),
+            SubPolicy(0.9, "invert", 4, 0.6, "equalize", 7, fillcolor),
+            SubPolicy(0.3, "contrast", 3, 0.8, "rotate", 4, fillcolor),
 
-            SubPolicy(0.8, "invert", 5, 0.0, "translateY", 2),
-            SubPolicy(0.7, "shearY", 6, 0.4, "solarize", 8),
-            SubPolicy(0.6, "invert", 4, 0.8, "rotate", 4),
-            SubPolicy(0.3, "shearY", 7, 0.9, "translateX", 3),
-            SubPolicy(0.1, "shearX", 6, 0.6, "invert", 5),
+            SubPolicy(0.8, "invert", 5, 0.0, "translateY", 2, fillcolor),
+            SubPolicy(0.7, "shearY", 6, 0.4, "solarize", 8, fillcolor),
+            SubPolicy(0.6, "invert", 4, 0.8, "rotate", 4, fillcolor),
+            SubPolicy(0.3, "shearY", 7, 0.9, "translateX", 3, fillcolor),
+            SubPolicy(0.1, "shearX", 6, 0.6, "invert", 5, fillcolor),
 
-            SubPolicy(0.7, "solarize", 2, 0.6, "translateY", 7),
-            SubPolicy(0.8, "shearY", 4, 0.8, "invert", 8),
-            SubPolicy(0.7, "shearX", 9, 0.8, "translateY", 3),
-            SubPolicy(0.8, "shearY", 5, 0.7, "autocontrast", 3),
-            SubPolicy(0.7, "shearX", 2, 0.1, "invert", 5)
+            SubPolicy(0.7, "solarize", 2, 0.6, "translateY", 7, fillcolor),
+            SubPolicy(0.8, "shearY", 4, 0.8, "invert", 8, fillcolor),
+            SubPolicy(0.7, "shearX", 9, 0.8, "translateY", 3, fillcolor),
+            SubPolicy(0.8, "shearY", 5, 0.7, "autocontrast", 3, fillcolor),
+            SubPolicy(0.7, "shearX", 2, 0.1, "invert", 5, fillcolor)
         ]
 
 
@@ -169,7 +168,7 @@ class SVHNPolicy(object):
 
 
 class SubPolicy(object):
-    def __init__(self, p1, operation1, magnitude_idx1, p2, operation2, magnitude_idx2):
+    def __init__(self, p1, operation1, magnitude_idx1, p2, operation2, magnitude_idx2, fillcolor=(128, 128, 128)):
         ranges = {
             "shearX": np.linspace(0, 0.3, 10),
             "shearY": np.linspace(0, 0.3, 10),
@@ -187,20 +186,26 @@ class SubPolicy(object):
             "invert": [0] * 10
         }
 
+        # from https://stackoverflow.com/questions/5252170/specify-image-filling-color-when-rotating-in-python-with-pil-and-setting-expand
+        def rotate_with_fill(img, magnitude):
+            rot = img.convert("RGBA").rotate(magnitude)
+            return Image.composite(rot, Image.new("RGBA", rot.size, (128,) * 4), rot).convert(img.mode)
+
         func = {
             "shearX": lambda img, magnitude: img.transform(
                 img.size, Image.AFFINE, (1, magnitude * random.choice([-1, 1]), 0, 0, 1, 0),
-                Image.BICUBIC, fillcolor=(128, 128, 128)),
+                Image.BICUBIC, fillcolor=fillcolor),
             "shearY": lambda img, magnitude: img.transform(
                 img.size, Image.AFFINE, (1, 0, 0, magnitude * random.choice([-1, 1]), 1, 0),
-                Image.BICUBIC, fillcolor=(128, 128, 128)),
+                Image.BICUBIC, fillcolor=fillcolor),
             "translateX": lambda img, magnitude: img.transform(
                 img.size, Image.AFFINE, (1, 0, magnitude * img.size[0] * random.choice([-1, 1]), 0, 1, 0),
-                fillcolor=(128, 128, 128)),
+                fillcolor=fillcolor),
             "translateY": lambda img, magnitude: img.transform(
                 img.size, Image.AFFINE, (1, 0, 0, 0, 1, magnitude * img.size[1] * random.choice([-1, 1])),
-                fillcolor=(128, 128, 128)),
-            "rotate": lambda img, magnitude: img.rotate(magnitude * random.choice([-1, 1])),
+                fillcolor=fillcolor),
+            "rotate": lambda img, magnitude: rotate_with_fill(img, magnitude),
+            # "rotate": lambda img, magnitude: img.rotate(magnitude * random.choice([-1, 1])),
             "color": lambda img, magnitude: ImageEnhance.Color(img).enhance(1 + magnitude * random.choice([-1, 1])),
             "posterize": lambda img, magnitude: ImageOps.posterize(img, magnitude),
             "solarize": lambda img, magnitude: ImageOps.solarize(img, magnitude),
